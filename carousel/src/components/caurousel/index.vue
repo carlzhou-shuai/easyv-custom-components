83a0a18d7814a9e489d7845812b47bc9ab1706f2
zhouchun <zhouchun@fulan.com.cn>
2023-05-19 17:12:25 +0800

    zc:完成旋转木马

---
+++ carousel/src/components/caurousel/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div
+    class="__easyv-component"
+    v-bind:id="id"
+    v-bind:style="{
+      position: 'absolute',
+      width: width + 'px',
+      height: height + 'px',
+      left: left + 'px',
+      top: top + 'px',
+      textAlign: 'center'
+    }"
+  >
+    <div class="swiper-container" ref="mySwiper" style="{height,width}">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" style="text-align: center">
+          <span>1</span>
+        </div>
+        <div class="swiper-slide" style="text-align: center">
+          <span>2</span>
+        </div>
+        <div class="swiper-slide" style="text-align: center">
+          <span>3</span>
+        </div>
+        <div class="swiper-slide" style="text-align: center">
+          <span>4</span>
+        </div>
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+import { reduceConfig } from './utils/reduce-config';
+import Swiper from 'swiper/swiper-bundle.min.js';
+import 'swiper/swiper-bundle.min.css';
+
+export default {
+  props: ['id', 'left', 'top', 'width', 'height', 'configuration', 'data'],
+  data() {
+    return {};
+  },
+  mounted() {
+    this.setSwiper();
+  },
+  computed: {
+    config: function () {
+      return reduceConfig(this.configuration);
+    }
+  },
+  methods: {
+    // 初始化swiper
+    setSwiper() {
+      console.log(1111);
+      new Swiper('.swiper-container', {
+        autoplay: true,
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          type: 'progressbar'
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.swiper {
+  width: 600px;
+  height: 300px;
+}
+</style>
---
+++ carousel2/src/components/carousel2/index.jsx
@@ -0,0 +1,102 @@
+import React,{useEffect,useRef} from "react";
+import css from "./styles/index.module.css";
+import Swiper from 'swiper/swiper-bundle.min.js';
+import 'swiper/swiper-bundle.min.css';
+import './styles/global.css'
+export default function (props) {
+  const { left, top, width, height, id, data, configuration: { series, max } } = props;
+  const mySwiperRef = useRef(null);
+  // 自定义逻辑
+  const styles = {
+    position: "absolute",
+    left,
+    top,
+    width,
+    height,
+    textAlign: "center",
+  };
+  // 把configuration.series obj转换为数组
+  const pages = JSON.parse(JSON.stringify(Object.keys(series).map(v => series[v])))
+    //截取关键字拼接HTML
+  pages.forEach(i => {
+        //i 一个卡片
+        const text = i.content;
+        const keywords = i.keyCode.split(',');
+        // 将不同关键字拼接成正则表达式
+        const regex = new RegExp(keywords.join("|"), "g");
+        // 使用正则表达式进行文本匹配
+        const matches = text.match(regex) || [];
+        // 将关键字和非关键字分别存放在不同的元素中
+        const result = text.split(regex).flatMap((val, index) => {
+          // 如果是关键字，则加入结果数组，并进行标记
+          if (index < matches.length) {
+            if (val) {
+              return [{
+                text: val,
+                isKeyword: false,
+              }, {
+                text: matches[index],
+                isKeyword: true,
+              }];
+            } else {
+              // 如果遇到连续的关键字，则只添加一个带标记的对象
+              return [{
+                text: matches[index],
+                isKeyword: true,
+              }];
+            }
+          } else if (val) {
+            // 如果不是关键字，则直接加入结果数组
+            return [{
+              text: val,
+              isKeyword: false,
+            }];
+          }
+          // 过滤掉空字符串
+        }).filter(val => val.text !== '');
+        // console.log(result,'result'); 
+        i.html = <div className={css['my-carousel-page-box-text']}>
+          {
+            result.reduce((a, v) => {
+              return <>{a} {v.isKeyword ? <a>{v.text}</a> : <span>{ v.text}</span>}</>
+            },[])
+          }
+        </div>
+        console.log(i.html,'html');
+  })
+  useEffect(() => {
+    mySwiperRef.current && mySwiperRef.current.destroy()
+    mySwiperRef.current = new Swiper('.swiper-container', {
+      autoplay: pages.length>max,
+      loop: pages.length>max,
+      slidesPerView: pages.length > max ? max : pages.length,
+      spaceBetween: 30,
+      pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+      }
+    });
+  },[props])
+  /**
+   * 固定格式: 返回的div必须有classNameName/id属性
+   */
+  return (
+    <div className="__easyv-component" style={styles} id={id}>
+       <div className="swiper-container"  style={{height,width}} ref={() => {}}>
+        <div className="swiper-wrapper">
+          {
+            pages.map((v,i) => {
+              return <div className="swiper-slide" key={i}>
+                <span className={css['backgroudImg']}>
+                  {v.html}
+                </span>
+              </div>
+            })
+          }
+    
+        </div>
+        <div className="swiper-pagination"></div>
+    </div>
+    </div>
+  );
+}
